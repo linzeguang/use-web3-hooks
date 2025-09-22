419401b170ec9f8ceecd4f1c6086c4411c42a054
linzeguang <15817289730@163.com>
2025-09-23 01:59:48 +0800

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,44 @@
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      include: ['src'],
+      exclude: ['src/examples'],
+      outDir: 'dist',
+      staticImport: true,
+      insertTypesEntry: true
+    })
+  ],
   build: {
     lib: {
-      entry: './src/index.ts',
+      entry: resolve(__dirname, 'src/index.ts'),
       name: 'useWeb3Hooks',
-      fileName: (format) => `use-web3-hooks.${format}.js`,
-      formats: ['es', 'cjs']
+      fileName: (format) => {
+        console.log('>>>>>> format: ', format)
+        return `index.${format}.js`
+      },
+      formats: ['es', 'cjs', 'umd']
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
       output: {
         globals: {
-          react: 'React'
-        }
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        },
+        // 优化 chunk 分割
+        manualChunks: undefined,
+        inlineDynamicImports: true
+        // // 减少文件数量
+        // chunkFileNames: 'chunks/[name]-[hash].js',
+        // entryFileNames: '[name].js',
+        // assetFileNames: 'assets/[name]-[hash][extname]'
       }
     }
   }
